e726408638ad656c250f4e015d05b1896d97ed09
Ilya <ilasevelev10776@gmail.com>
2022-11-26 13:43:05 +0300

    first commit

---
+++ chat/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+from chat.models import Room
+from users.models import CustomUser
+
+
+def index_view(request):
+    return render(
+        request,
+        "index.html",
+        {
+            "rooms": Room.objects.all(),
+            "users": CustomUser.objects.all(),
+        },
+    )
+
+
+def room_view(request, room_name):
+    chat_room, created = Room.objects.get_or_create(name=room_name)
+    return render(
+        request,
+        "room.html",
+        {
+            "room": chat_room,
+        },
+    )
